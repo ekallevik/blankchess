f0f31273a630033d8992b5ede76661efec2805d8
Even Kallevik <even.kallevik@gmail.com>
2020-12-04 14:54:52 +0100

    Create Game-class

--- main.py
+++ main.py
@@ -47,22 +47,29 @@ class AIPlayer(Player):
         self.perform_move(move)
 
 
-def play():
+class Game:
 
-    board = chess.Board()
-    white = HumanPlayer(board, chess.WHITE)
-    black = AIPlayer(board, chess.BLACK)
+    def __init__(self, starting_player):
 
-    current_player = white
+        self.board = chess.Board()
+        self.human = HumanPlayer(self.board, chess.WHITE if starting_player == "human" else chess.BLACK)
+        self.ai = AIPlayer(self.board, chess.BLACK if starting_player == "human" else chess.WHITE)
 
-    while not board.is_game_over(claim_draw=False):
-        print(board)
-        print(f"{current_player.get_color()} to move.")
-        current_player.move()
-        current_player = black if current_player == white else white
-        print()
+        self.current_player = self.human if starting_player == "human" else self.ai
 
+    def play(self):
+        while not self.board.is_game_over(claim_draw=False):
+            print(self.board)
+            print(f"{self.current_player.get_color()} to move.")
+            self.current_player.move()
+            self.update_current_player()
+            print()
+
+    def update_current_player(self):
+        self.current_player = self.ai if self.current_player == self.human else self.human
 
 if __name__ == '__main__':
-    play()
+    game = Game("human")
+    game.play()
+
 
