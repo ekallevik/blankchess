45111dd645a88f0b17ff12c2fe9eccd528b546a2
Even Kallevik <even.kallevik@gmail.com>
2020-12-04 15:28:33 +0100

    Add calculate_fitness()

--- player.py
+++ player.py
@@ -2,6 +2,15 @@ from abc import ABC
 
 import chess
 
+MATERIAL_SCORE = {
+    chess.PAWN: 10,
+    chess.KNIGHT: 30,
+    chess.BISHOP: 30,
+    chess.ROOK: 50,
+    chess.QUEEN: 90,
+    chess.KING: 900,
+}
+
 
 class Player(ABC):
 
@@ -18,3 +27,14 @@ class Player(ABC):
 
     def get_color(self):
         return "WHITE" if self.color == chess.WHITE else "BLACK"
+
+    def calculate_fitness(self):
+
+        score = 0
+
+        for piece, value in MATERIAL_SCORE.items():
+            squares = self.board.pieces(piece, self.color)
+            score += value * len(squares)
+
+        return score
+
